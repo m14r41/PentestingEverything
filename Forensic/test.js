b64cdb65a55d65f81dbf9a8235f194236e296093
InfoBugs <96067790+InfoBugs@users.noreply.github.com>
2024-04-30 00:20:32 +0530

    Create test.js (#29)

---
+++ Forensic/test.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Expressions</h2>
+
+<p>Expressions compute to values.</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = 5 * 10;
+</script>
+
+</body>
+</html>
